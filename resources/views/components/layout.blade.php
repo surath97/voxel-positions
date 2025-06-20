37362400d9176ceecf861cdabda4904788aecdff
Surath Chathuranga <surathchathuranga97@gmail.com>
2025-06-20 23:00:18 +0530

    Add Hanken Grotesk font

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,9 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Voxel Positions</title>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Hanken+Grotesk:wght@400;500;600&display=swap"
+        rel="stylesheet">
     @vite(['resources/css/app.css','resources/js/app.js'])
 </head>
-<body class="bg-pixel_positions text-white">
+<body class="bg-pixel_positions text-white font-hanken-grotesk">
     
     <div class="px-10">
         <nav class="flex justify-between items-center py-4">
