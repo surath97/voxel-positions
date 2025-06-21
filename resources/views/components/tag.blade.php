59d8e3170ec93f4d7847488e14f297458e403d93
Surath Chathuranga <surathchathuranga97@gmail.com>
2025-06-21 21:15:21 +0530

    Create new text class and use it on tags

--- resources/views/components/tag.blade.php
+++ resources/views/components/tag.blade.php
@@ -1,1 +1,1 @@
-<a href="" class="bg-white/10 px-2 py-1 rounded-xl text-xs hover:bg-white/25 transition-colors duration-300">{{ $slot }}</a>
+<a href="" class="bg-white/10 px-3 py-1 rounded-xl text-2xs font-bold hover:bg-white/25 transition-colors duration-300">{{ $slot }}</a>
