936da333ea8c2b7416e12500daaf334ac1e9e086
Surath Chathuranga <surathchathuranga97@gmail.com>
2025-06-21 22:15:04 +0530

    Create search section UI

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,7 +2,16 @@
 
     <div class="space-y-10">
         {{-- Section 1 --}}
-        <section>
+        <section class="text-center pt-6">
+            <h1 class="font-bold text-4xl">Let's Find Your Next Job</h1>
+
+            <form action="" class="mt-6">
+                <input type="text" placeholder="Web Developer..." class="rounded-2xl bg-white/5 border-white/10 px-5 py-4 w-full max-w-xl">
+            </form>
+        </section>
+
+        {{-- Section 2 --}}
+        <section class="pt-10">
             <x-section-heading>Featured Jobs</x-section-heading>
             <div class="grid lg:grid-cols-3 gap-8 mt-6">
                 <x-job-card />
@@ -11,7 +20,7 @@
             </div>
         </section>
 
-         {{-- Section 2 --}}
+         {{-- Section 3 --}}
         <section>
             <x-section-heading>Tags</x-section-heading>
 
@@ -29,7 +38,7 @@
             </div>
         </section>
 
-         {{-- Section 3 --}}
+         {{-- Section 4 --}}
         <section>
             <x-section-heading>Recent Jobs</x-section-heading>
 
