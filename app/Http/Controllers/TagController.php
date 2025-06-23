ccf3349dcbd6de67150bb45b01b5d6f3d9ba36be
Surath Chathuranga <surathchathuranga97@gmail.com>
2025-06-23 14:22:03 +0530

    Make tag model and assign relations

--- app/Http/Controllers/TagController.php
+++ app/Http/Controllers/TagController.php
@@ -2,18 +2,18 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Job;
-use App\Http\Requests\StoreJobRequest;
-use App\Http\Requests\UpdateJobRequest;
+use App\Models\Tag;
+use App\Http\Requests\StoreTagRequest;
+use App\Http\Requests\UpdateTagRequest;
 
-class JobController extends Controller
+class TagController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
     public function index()
     {
-        return view('jobs.index');
+        //
     }
 
     /**
@@ -27,7 +27,7 @@ class JobController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(StoreJobRequest $request)
+    public function store(StoreTagRequest $request)
     {
         //
     }
@@ -35,7 +35,7 @@ class JobController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(Job $job)
+    public function show(Tag $tag)
     {
         //
     }
@@ -43,7 +43,7 @@ class JobController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(Job $job)
+    public function edit(Tag $tag)
     {
         //
     }
@@ -51,7 +51,7 @@ class JobController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(UpdateJobRequest $request, Job $job)
+    public function update(UpdateTagRequest $request, Tag $tag)
     {
         //
     }
@@ -59,7 +59,7 @@ class JobController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Job $job)
+    public function destroy(Tag $tag)
     {
         //
     }
